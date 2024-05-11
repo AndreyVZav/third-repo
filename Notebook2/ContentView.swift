1aee6fa893b4cb639b24f33837db18764091f99d
Andrey <avzavadskiy@gmail.com>
2024-05-11 19:54:38 +0300

    Initial Commit

---
+++ Notebook2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Notebook2
+//
+//  Created by Андрей Завадский on 11.05.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Notebook2/Notebook2App.swift
@@ -0,0 +1,17 @@
+//
+//  Notebook2App.swift
+//  Notebook2
+//
+//  Created by Андрей Завадский on 11.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct Notebook2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
